578200d2490567027816fd2433a7d2d538bdc454
Dika Afty <dikaafty@gmail.com>
2024-09-06 14:46:19 +0700

    Add Loop For Adding Square Divs

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 const board = document.querySelector(".board");
 board.style.gridTemplateColumns = "repeat(16, 1fr)";
 board.style.gridTemplateRows = "repeat(16, 1fr)";
+
+for(let i = 0; i < 256; i++) {
+  let square = document.createElement("div");
+  square.style.backgroundColor = "red";
+  board.insertAdjacentElement("beforeend", square);
+}
