5f6e08a9f680dea264c35a8aab5734471f1b94b6
Dika Afty <dikaafty@gmail.com>
2024-09-06 15:34:16 +0700

    Add Change Size Func And Its Logic

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 function populateBoard(size) {
   const board = document.querySelector(".board");
+  let squares = board.querySelectorAll("div");
+  squares.forEach((div) => div.remove());
   board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
   board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-  for(let i = 0; i < 256; i++) {
+  let amount = size * size;
+  for(let i = 0; i < amount; i++) {
     let square = document.createElement("div");
     square.style.backgroundColor = "red";
     board.insertAdjacentElement("beforeend", square);
@@ -11,3 +14,13 @@ function populateBoard(size) {
 }
 
 populateBoard(16);
+
+function changeSize(input) {
+  if(input >= 2 && input <= 100) {
+  populateBoard(input);
+  } else if(input < 2) {
+    alert("Too few squares");
+  } else if(input > 100) {
+    alert("Too many squares");
+  }
+}
